b09fc69710f9febdf4f849de3524a2829c03e822
Dwi Wijonarko <d.wijonarko@gmail.com>
2021-04-09 22:25:48 +0700

    update readme

--- resources/views/livewire/chain-dropdown.blade.php
+++ resources/views/livewire/chain-dropdown.blade.php
@@ -1,12 +1,13 @@
-<div>
-    <select name="provices" id="provinces" wire:model="provinceId">
+<div class="row">
+    <select name="provices" id="provinces" wire:model="provinceId" class="col-md-4 form-control">
         <option value="">Select Province</option>
         @foreach ($provinces as $province)
             <option value="{{ $province->id }}">{{ $province->name }}</option>
         @endforeach
     </select>
     @if (!is_null($provinceId))
-        <select>
+        <div class="col-md-1"></div>
+        <select class="col-md-4 form-control">
             <option value="" selected>Select City</option>
             @foreach ($cities->cities as $city)
                 <option value="{{ $city->id }}">{{ $city->name }}</option>
